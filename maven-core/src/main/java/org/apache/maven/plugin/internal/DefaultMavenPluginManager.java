502bd934976aa7702e07f82b38681e803b58efae
Hervé Boutemy <hboutemy@apache.org>
2022-03-26 04:37:07 +0100

    [MNG-7438] rephrase "Configuring" to "Loading" mojo "from plugin realm"
    to differentiate from "configuring mojo" = apply configuration
    
    rename populatePluginFields(..) private method
    to populateMojoExecutionFields(..) to better match the intent

--- maven-core/src/main/java/org/apache/maven/plugin/internal/DefaultMavenPluginManager.java
+++ maven-core/src/main/java/org/apache/maven/plugin/internal/DefaultMavenPluginManager.java
@@ -515,7 +515,7 @@ public class DefaultMavenPluginManager
 
         if ( logger.isDebugEnabled() )
         {
-            logger.debug( "Configuring mojo " + mojoDescriptor.getId() + " from plugin realm " + pluginRealm );
+            logger.debug( "Loading mojo " + mojoDescriptor.getId() + " from plugin realm " + pluginRealm );
         }
 
         // We are forcing the use of the plugin realm for all lookups that might occur during
@@ -609,8 +609,8 @@ public class DefaultMavenPluginManager
 
             ExpressionEvaluator expressionEvaluator = new PluginParameterExpressionEvaluator( session, mojoExecution );
 
-            populatePluginFields( mojo, mojoExecution.getExecutionId(), mojoDescriptor, pluginRealm, pomConfiguration,
-                                  expressionEvaluator );
+            populateMojoExecutionFields( mojo, mojoExecution.getExecutionId(), mojoDescriptor, pluginRealm,
+                                         pomConfiguration, expressionEvaluator );
 
             return mojo;
         }
@@ -621,9 +621,9 @@ public class DefaultMavenPluginManager
         }
     }
 
-    private void populatePluginFields( Object mojo, String executionId, MojoDescriptor mojoDescriptor,
-                                       ClassRealm pluginRealm, PlexusConfiguration configuration,
-                                       ExpressionEvaluator expressionEvaluator )
+    private void populateMojoExecutionFields( Object mojo, String executionId, MojoDescriptor mojoDescriptor,
+                                              ClassRealm pluginRealm, PlexusConfiguration configuration,
+                                              ExpressionEvaluator expressionEvaluator )
         throws PluginConfigurationException
     {
         ComponentConfigurator configurator = null;
